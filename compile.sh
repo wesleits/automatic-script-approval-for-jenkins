dca4ff1f1130b9d4a2d68ab63216373e492a9404
Weslei Teixeira da Silveira <wesleiteixeira@hotmail.com.br>
2017-11-08 23:40:23 +0000

    scripts updated

---
+++ compile.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+##############################################################################################################################################################################
+##### Script works correct in Ubuntu 14/16                                                                                                                               #####
+##### To installs this plugin runs in jenkins machine these commands:                                                                                                    #####
+#####                                                                                                                                                                    #####
+##### wget https://raw.githubusercontent.com/wesleits/automatic-script-approval-for-jenkins/master/automatic-script-approval-for-jenkins.hpi -P /var/lib/jenkins/plugins #####
+##### service jenkins restart                                                                                                                                            ##### 
+##############################################################################################################################################################################
+
+
+if [ ! -d ".git" ]
+then
+  #=== Elevate script to root if not is
+  if [ $EUID != 0 ]
+  then 
+    sudo "$0" "$@"
+    exit $?
+  else
+    #=== Install pre requisites
+    apt-get update
+    apt-get -y install software-properties-common
+    apt-get -y install wget
+    apt-get -y install vim
+
+    #=== Install git
+    add-apt-repository -y ppa:git-core/ppa
+    apt-get update
+    apt-get -y install git
+    
+    #=== Install JDK 8
+    add-apt-repository -y ppa:webupd8team/java
+    echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
+    apt-get update
+    apt-get install -y oracle-java8-installer
+    
+    #=== Install Maven 3
+    apt-get purge maven maven2 maven3
+    add-apt-repository ppa:andrei-pozolotin/maven3
+    apt-get update
+    apt-get -y install maven3
+  fi
+
+  #== initialize repository
+  git init
+  git config --global core.editor "vim"
+  git config --local core.sshCommand "ssh -i /var/tmp/weslei -o StrictHostKeyChecking=no"
+  git remote add origin git@github.com:wesleits/automatic-script-approval-for-jenkins.git
+  git config user.name "Weslei Teixeira da Silveira"
+  git config user.email "wesleiteixeira@hotmail.com.br"
+fi
+
+if [ -z "$1" ]
+then
+  MESSAGE="scripts updated"
+else
+  MESSAGE=$1
+fi
+   
+git fetch
+git checkout master
+git merge
+
+mvn package  # needs JDK 8 to correct compile plugin!!!
+
+#=== only push if the maven package does not to return some error
+if [ $? -eq 0 ]
+then
+  cp ./target/automatic-script-approval-for-jenkins.hpi ./
+
+  git add -A
+  git reset "./target"
+  git commit -m "$MESSAGE"
+  git push -f
+fi
+
+read
